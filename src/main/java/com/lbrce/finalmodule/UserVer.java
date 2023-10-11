2650cc541e73064fcb28414cbe4fd57d66c84742
kumpatisupriya <110220533+kumpatisupriya@users.noreply.github.com>
2023-10-11 15:03:41 +0530

    Add files via upload

---
+++ src/main/java/com/lbrce/finalmodule/UserVer.java
@@ -0,0 +1,5 @@
+package com.lbrce.finalmodule;
+
+public class UserVer {
+
+}
---
+++ src/test/java/com/lbrce/finalmodule/UserVerificationTest.java
@@ -0,0 +1,23 @@
+package com.lbrce.finalmodule;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserVerificationTest {
+		UserVerification uv=new UserVerification();
+		@Test
+		public void testcase1() {
+			Assert.assertEquals(true,uv.check("usha", "usha@2020"));
+		}
+		@Test
+		public void testcase2() {
+			Assert.assertEquals(true,uv.check("usha", "usha@2020"));
+		}
+		@Test
+		public void testcase3() {
+			Assert.assertEquals(false,uv.check("usha", "usha@202"));
+		}
+		@Test
+		public void testcase4() {
+			Assert.assertEquals(false,uv.check("ush", "usha@2020"));
+		}
+}
